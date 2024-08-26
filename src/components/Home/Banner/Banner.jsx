9008c5b99272f5ed986b5a859de6d60a2d5e2625
Suyog Chavan <chavansuyog2005@gmail.com>
2024-08-26 14:15:00 +0530

    Add files via upload

---
+++ src/components/Home/Banner/Banner.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import "./Banner.scss";
+import BannerImg from "../../../assets/banner-img.png";
+
+const Banner = () => {
+    return (
+        <div className="hero-banner">
+            <div className="content">
+                <div className="text-content">
+                    <h1>SALES</h1>
+                    <p>
+                        The new powerfull base Headphone with bluetooth5.3 and fast connectivity
+                        with 4.5mm jack. 
+                    </p>
+                    <div className="ctas">
+                        <div className="banner-cta">Read More</div>
+                        <div className="banner-cta v2">Shop Now</div>
+                    </div>
+                </div>
+                <img className="banner-img" src={BannerImg} />
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
